63c7188c30d9b2f7393178e7c0c2c42ed0bf5183
Alish Shrestha <cresthaalish@gmail.com>
2025-02-13 14:50:16 +0545

    feat: Add Auth app

--- backend/CoreRoot/settings.py
+++ backend/CoreRoot/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'core',
-    'core.user'
+    'core.user',
+    'core.auth'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core_user.User'
+
+REST_FRAMEWORK={
+  'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework_simplejwt.authentication.JWTAuthentication',
+  ),
+  'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
--- backend/core/user/models.py
+++ backend/core/user/models.py
@@ -56,8 +56,14 @@ class User(AbstractBaseUser, PermissionsMixin):
   email = models.EmailField(db_index=True, unique=True)
   is_active = models.BooleanField(default=True)
   is_superuser = models.BooleanField(default=False)
+  
+  bio = models.TextField(null=True)
+  avatar = models.ImageField(null=True)
+  
   created = models.DateTimeField(auto_now=True)
   updated = models.DateTimeField(auto_now_add=True)
+
+ 
 
   USERNAME_FIELD = 'email'
   REQUIRED_FIELDS = ['username']
